9821c0cf8a267b205dc6bcedfc790d871c0a2b62
NishatFariza <nishatfarzia78@gmail.com>
2022-11-21 16:10:10 +0600

    wip:test tect

---
+++ src/components/Text.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Title } from "@mantine/core";
+
+
+
+const TextTest= () => {
+  return (
+    <div>
+      <>
+              <Title order={1}>{}</Title>
+        <Title order={2}>This is h2 title</Title>
+        <Title order={3}>This is h3 title</Title>
+        <Title order={4}>This is h4 title</Title>
+        <Title order={5}>This is h5 title</Title>
+        <Title order={6}>This is h6 title</Title>
+      </>
+    </div>
+  );
+};
+
+export default TextTest;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import TextTest from '../components/Text'
+
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
       <h1>Hello</h1>
+      <TextTest />
    </div>
   )
 }
