fa7de07c53be1ff4f8bc53f0985cf1785eab5f55
NishatFariza <nishatfarzia78@gmail.com>
2022-11-27 11:25:11 +0600

    wip:form validation

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,31 +2,42 @@ import { Button, Paper, PasswordInput, Space, TextInput } from "@mantine/core";
 import React from "react";
 import { z as zod } from "zod";
 import { useForm, zodResolver } from "@mantine/form";
+import { json } from "stream/consumers";
 
-const loginformSchema = zod.object({});
-
-const login = () => {
-  const form = useForm({
-    initialValues: {
-      email: "test@test.com",
-      password: "",
-    },
-
-    // functions will be used to validate values at corresponding key
-    validate: zodResolver({
-      name: zod
+const loginformSchema = zod.object({name: zod
         .string()
         .min(2, { message: "Name should have at least 2 letters" }),
       email: zod.string().email({ message: "Invalid email" }),
       password: zod.string().min(6).max(12)
-    }),
+    });
+
+const login = () => {
+  const form = useForm({
+    validate: zodResolver(loginformSchema),
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
   });
+
+  const handleSubmitForm = (values: any) => {
+     console.log(values);
+  }
+
   return (
     <div className=" h-screen bg-slate-200 ">
       <div className="max-w-lg mx-auto py-20">
         <Paper p={"lg"} radius={"md"}>
-          <form onSubmit={form.onSubmit(console.log)}>
-            <TextInput label="Name" placeholder="Name" required />
+          <pre>{ JSON.stringify(form.values, null, 2)}</pre>
+          <pre>{ JSON.stringify(form.errors, null, 2)}</pre>
+          <form onSubmit={form.onSubmit(handleSubmitForm)}>
+            <TextInput
+              label="Name"
+              placeholder="Name"
+              required
+              {...form.getInputProps("name")}
+            />
             <Space h={"sm"} />
             <TextInput
               required
@@ -35,7 +46,12 @@ const login = () => {
               {...form.getInputProps("email")}
             />
             <Space h={"sm"} />
-            <PasswordInput placeholder="******" label="Password" withAsterisk />
+            <PasswordInput
+              placeholder="******"
+              label="Password"
+              withAsterisk
+              {...form.getInputProps("password")}
+            />
             <Space h={"sm"} />
             <Button
               variant="gradient"
