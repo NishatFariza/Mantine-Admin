313584a1e78bba8fafdde3dbe6547bde4d4d17db
NishatFariza <nishatfarzia78@gmail.com>
2022-11-27 09:00:37 +0600

    wip:login form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,10 @@
-import { Button, Container, NumberInput, Paper, TextInput } from "@mantine/core";
+import {
+  Button,
+  Container,
+  NumberInput,
+  Paper,
+  TextInput,
+} from "@mantine/core";
 import Head from "next/head";
 import Image from "next/image";
 import { NavbarNested } from "../components/Navbar";
@@ -21,31 +27,42 @@ export default function Home() {
   });
 
   return (
-    <div className="max-w-lg mx-auto shadow-xl px-16 py-10 mt-16">
-      <form onSubmit={form.onSubmit(console.log)}>
-        <TextInput
-          label="Name"
-          placeholder="Name"
-          {...form.getInputProps("name")}
-        />
-        <TextInput
-          mt="sm"
-          label="Email"
-          placeholder="Email"
-          {...form.getInputProps("email")}
-        />
-        <NumberInput
-          mt="sm"
-          label="Age"
-          placeholder="Age"
-          min={0}
-          max={99}
-          {...form.getInputProps("age")}
-        />
-        <Button color="gray" type="submit" mt="sm">
-          Submit
-        </Button>
-      </form>
+    <div className=" h-screen bg-slate-100 ">
+      <Container>
+        <Paper p={"lg"} radius={"md"}>
+          <div className="max-w-lg mx-auto">
+            <form onSubmit={form.onSubmit(console.log)}>
+              <TextInput
+                label="Name"
+                placeholder="Name"
+                {...form.getInputProps("name")}
+              />
+              <TextInput
+                mt="sm"
+                label="Email"
+                placeholder="Email"
+                {...form.getInputProps("email")}
+              />
+              <NumberInput
+                mt="sm"
+                label="Age"
+                placeholder="Age"
+                min={0}
+                max={99}
+                {...form.getInputProps("age")}
+              />
+              <Button
+                variant="gradient"
+                gradient={{ from: "teal", to: "lime", deg: 105 }}
+                type="submit"
+                mt="sm"
+              >
+                Submit
+              </Button>
+            </form>
+          </div>
+        </Paper>
+      </Container>
     </div>
   );
 }
